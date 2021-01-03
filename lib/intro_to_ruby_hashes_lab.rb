dfc7dea05c49752e4229c741cb176cacb7ab1edd
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 15:59:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -14,6 +14,7 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  number = 5
   id_hash_generator(5)[:id]
-  id_hash_generator(451)[:id]
+
 end
